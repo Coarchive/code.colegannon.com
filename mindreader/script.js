498af7c714c2d2b235e582bdf3c55ab895913a9c
legodude17 <joshuadb@me.com>
2016-09-24 20:51:46 -0700

    Add battery

---
+++ mindreader/script.js
@@ -0,0 +1,38 @@
+/*
+TODO:
++ Permissons
++ Screen Stuff
++ Battery -
++ Stats of website
+*/
+var battery;
+navigator.getBattery().then(function (batteryManager) {
+  battery.charging = batteryManager.charging;
+  battery.percent = batteryManager.level * 100;
+  if (battery.charging) {
+    battery.time = batteryManager.chargingTime;
+  } else {
+    battery.time = batteryManager.dischargingTime;
+  }
+  batteryManager.onlevelchange = function () {
+    battery.percent = batteryManager.level * 100;
+  };
+  batteryManager.onchargingchange = function () {
+    battery.charging = batteryManager.charging;
+    if (battery.charging) {
+      battery.time = batteryManager.chargingTime;
+    } else {
+      battery.time = batteryManager.dischargingTime;
+    }
+  };
+  batteryManager.onchargingtimechange = batteryManager.ondischargingtimechange = function () {
+    if (battery.charging) {
+      battery.time = batteryManager.chargingTime;
+    } else {
+      battery.time = batteryManager.dischargingTime;
+    }
+  }
+}, function (err) {
+  console.error('Failed to get Battery');
+  console.error(err);
+});
