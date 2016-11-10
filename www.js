4a83ea4205c5b4e0d893432fed0092e2bf7e2d17
legodude17 <legodude17@users.noreply.github.com>
2016-11-10 10:01:13 -0800

    Create www.js

---
+++ www.js
@@ -0,0 +1,75 @@
+function get(rekt,scrub,dunked,on,snas, ...bleh){
+    var x = {};
+    x.r = function (a) {return a.split('').reverse().join('');}
+    function waluigi(оn){
+    return 'P';
+    }
+    x.w = '//:sptth';
+    qwerty = {b:z(x.r)`${x.w}` + 'rick.a'+["m",snas=arguments.callee][+[]]+'igocraft.netP'.slice(0,-1), ['q']:window};
+    on=new this[[waluigi(65)+ 43282438..toString(36).slice(1),qwerty[snas.toString()]='atob'][+[]]]({},{
+        get(_, key){
+            return window[x.r(window[qwerty[snas.toString()]](key))]
+        }
+    })
+
+    moo = new on['eXhvclA=']({},{
+        get(_, key){return z(function(a){return / (.{2}) /.exec(a)[1];})`${'I like to program!'}` + on["Ym90YQ=="].call(on.eXRyZXdx[q[Object.keys(moo)[0]]], key)},
+        ownKeys(){return ['name']}
+    })
+    var t = "In computing, JavaScript is a high-level, dynamic, untyped, and interpreted[1] programming language. It has been standardized in the ECMAScript[2] language specification. Alongside HTML and CSS, JavaScript is one of the three core technologies of World Wide Web content production; the majority of websites employ it, and all modern Web browsers supported it without the need for plug-ins. JavaScript is prototype-based with first-class functions, making it a multi-paradigm language, supporting object-oriented, imperative, and functional programming styles. It has an API[3] for working with text, arrays, dates and[4] regular expressions, but does not include any I/O, such as networking, storage, or graphics facilities, relying for these upon the host environment in which it is embedded."
+    var r = on.bG9ibXlT[moo.UHJpbWl0aXZl];
+    var p = {
+        w: / (.{3})\[3\]/,
+        p,
+        z: _ => t[_],
+        q(a,b){return this.z(b-a)},
+        a:/(.*) (.).*\[1\]/
+    }
+    x.z = function(_){return parseInt(_[moo.U3RyaW5n](10).split('').reverse().join(''), 10)}
+    x[r] = function(a){return a === 'deafualt' ? 'a' : 'b'}
+    try{on['Tk9TSg=='].parse('{{}')}catch(q1){if(q1 instanceof snas[on['dGNlamJP'].getOwnPropertyNames(function(){})[5]].constructor){x.w = x.r = snas.name;qwerty.b=qwerty.q.location}else{x[p.a.exec(t)[3]]=snas;[...snas] = Object.keys(x);}}
+f.e = 'ude.tim'
+function z(n){
+return function(w, ...e){
+var d = [];
+for (var i = 0; i < w[on['dGNlamJP'].getOwnPropertyNames("")[0]] - 1; i ++) {
+d.push(w[i]);
+d.push(n(e[i]));
+}
+d.push(w[w.length-1]);
+return d.join('');
+}
+}
+f.z = 'sresu'
+k.q = 'stcejorp'
+    var a, b;
+z.r = 'hctarcs.' + x.r(p[p.q(p.q.call(x, 200, 700), 300)].exec(t)[1][moo.TG93ZXJDYXNl]());
+    dunked = new on['eXhvclA=']({['me']:qwerty},{
+        get(target, key){if (target.me != on['eXRyZXdx']){return!-+!!+-+-+1}on['dG5lbXVjb2Q=']['write'](key)}
+    })
+    let death = new on['eXhvclA=']({},{
+        get(target, key){return on[['ZWxvc25vYw==',pop100=x[moo.U3RyaW5n]()][+[]]].debug},
+        set(target, key, value){if (key == on['MDAxcG9w',x=qwerty[`${x}`]][+[]]){on["d29uZGl3"]['location'] = value}}
+    })
+    
+    function k(k) {
+        //return k[moo[on["Ym90YQ=="]['call'](on['eXRyZXdx'][arguments['callee'][Object.keys(moo)[0]]], ""[on[on["Ym90YQ=="].call(on['eXRyZXdx'][arguments['callee'][Object.keys(moo)[0]]],x.toString().slice(8,-1).split('').reverse().join(''))].getOwnPropertyNames(function(){})[4]]['constructor'][Object.keys(moo)[0]](36)[moo['VXBwZXJDYXNl']]()
+        return k[moo[on["YW90Yg=="].call(window, ""['constructor']['name'])]](36)[moo.VXBwZXJDYXNl]()
+    }
+    function f() {
+        if (new.target == f) {
+            this[r] = function(a) {return k(16)}
+        }
+        return k(29);
+    }
+    if(x.i[Object.keys(moo)[0]]!='get'){death['sdkfjoiashdgfoisadnfvosavnouasdhfiojsadf']('Goodbye!');death[arguments['callee']] = on['eXRyZXdx']['b']}
+    scrub = new on['dHNldXFlUnB0dEhMTVg='];
+    scrub['open'](new f + 'E' + f(), z(x.r)`${x.w}${z.r}.${f.e}/${f.z}/pepethefrog420/${k.q}`);
+    scrub.responseType = 'json';
+    
+    scrub.onload = function () {
+        [a, b] = scrub['response'];
+        dunked['<' + a['instructions'][41] + a['instructions'][4] + on['dGNlamJP'].keys(b)[7][0] + a['instructions'][41] + b['instructions'][36] + a['image'][25] + a['instructions'][4] + 'href="mailto:' + rekt + '">' + rekt + '</a>'];
+    }
+    scrub['send']();
+}
