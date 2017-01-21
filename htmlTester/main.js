02303f2939f08d193fe74220325b800eb2259499
Coalpha <Arcanehood@gmail.com>
2017-01-21 15:42:33 -0800

    i broke it

--- htmlTester/main.js
+++ htmlTester/main.js
@@ -1,10 +1,139 @@
-/*jshint ignore:start*/
-var txt;if(location.hash)document.getElementById("foo").value=location.hash.slice(1);else if(txt=localStorage.getItem("lcltext"))document.getElementById("foo").value=txt;
-function parseCstm(a){document.getElementById("a").value=a;a=a.split(";");document.getElementById("1").style.backgroundColor=a[0];document.getElementById("2").style.backgroundColor=a[1];document.getElementById("q").style.backgroundColor=a[2];document.getElementById("r").style.backgroundColor=a[3];document.getElementById("d").style.backgroundColor=a[4];document.getElementById("h").style.backgroundColor=a[5];document.body.style.color=a[6]}var gd;(gd=localStorage.getItem("hmtltseter"))&&parseCstm(gd);
-var t=!1,hide;function toggleRemove(){var a=document.getElementById("q"),b=document.getElementById("d"),c=document.getElementById("h");t?(a.style.right=17,b.style.opacity=0,c.style.opacity=0,hide=setTimeout(function(a){b.style.display=c.style.display="none"},1E3),t=!1):(b.style.display=c.style.display="block",a.style.right=87,clearTimeout(hide),b.style.opacity=1,c.style.opacity=1,t=!0)}
-function removeLocalTextarea(){document.getElementById("foo").value="";localStorage.removeItem("lcltext");location.reload()}function removeLocals(){document.getElementById("foo").value="";localStorage.removeItem("lcltext");localStorage.removeItem("hmtltseter");localStorage.removeItem("mnuul");location.reload()}String.prototype.splice||(String.prototype.splice=function(a,b,c){return this.slice(0,a)+c+this.slice(a+Math.abs(b))});document.title="HTML Tester";var foo=document.getElementById("foo");
-foo.onkeydown=function(a){setTimeout(function(){localStorage.setItem("lcltext",foo.value)},100);"Tab"==a.key&&(a.preventDefault(),foo.value=foo.value.splice(a=foo.selectionStart,0,"    "),foo.selectionStart=foo.selectionEnd=a+4)};
-function test(){document.getElementById("q");var a=foo.value,b;try{preWindow.location.host}catch(c){b=c&&!1}"undefined"===typeof b&&(b=!0);window.preWindow&&!window.preWindow.closed&&b||(window.preWindow=open());window.preWindow.document.open();window.preWindow.document.write(a);window.preWindow.document.close();a||(window.preWindow.location="/dogrolld");window.preWindow.focus();window.onfocus=function(){foo.select()}}mnu=!1;
-function openMenu(){document.getElementById("instructions").innerHTML="Status;Title;Test;Remove;Trash;Locals;";localStorage.setItem("mnuul",!0);var a=document.getElementsByClassName("title")[0].style;a.height="50%";a.fontSize="3vw";a.alignItems="flex-start";a.justifyContent="flex-end";a.padding="10";a.top="30px";document.getElementById("a").style.display="block";mnu=!0}function toggleMenu(){mnu?closeMenu():localStorage.getItem("mnuul")&&openMenu()}
-function closeMenu(){if(mnu){var a=document.getElementsByClassName("title")[0].style;a.height="20%";a.fontSize="6vw";a.alignItems="center";a.justifyContent="center";a.padding="0";a=document.getElementById("a");a.style.display="none";a.value&&(parseCstm(a.value),localStorage.setItem("hmtltseter",a.value));mnu=!1}}var recognition=new webkitSpeechRecognition;recognition.onresult=function(a){"they're taking the Hobbits to Isengard"==a.results["0"]["0"].transcript&&openMenu()};recognition.start();
-/*jshint ignore:end*/
+/*jshint esversion:6*/
+/* Warning, this code is really poorly coded the structure is worse than Marcus' code but it's a little more readable.
+It's also a little more stupid.
+*/
+var txt;
+if(location.hash){
+document.getElementById('foo').value=location.hash.slice(1);
+}else if(txt=localStorage.getItem('lcltext')){//jshint ignore:line
+    document.getElementById('foo').value=txt;
+}
+    function parseCstm(ste){
+        document.getElementById('a').value=ste;
+        var c = ste.split(';');
+        document.getElementById('1').style.backgroundColor=c[0];
+        document.getElementById('2').style.backgroundColor=c[1];
+        document.getElementById('q').style.backgroundColor=c[2];
+        document.getElementById('r').style.backgroundColor=c[3];
+        document.getElementById('d').style.backgroundColor=c[4];
+        document.getElementById('h').style.backgroundColor=c[5];
+        document.body.style.color=c[6];
+    }
+    var gd;
+    if(gd=localStorage.getItem('hmtltseter')){//jshint ignore:line
+        parseCstm(gd);
+    }
+    var t=false,hide;
+    function toggleRemove(){
+        var dfs = document.getElementById('q');
+        var rtxt = document.getElementById('d');
+        var rall = document.getElementById('h');
+        if(t){
+            dfs.style.right=17;
+            rtxt.style.opacity=0;
+            rall.style.opacity=0;
+            hide=setTimeout(_=>{rtxt.style.display=rall.style.display='none';},1000);
+            t=false;
+        }else{
+            rtxt.style.display=rall.style.display='block';
+            dfs.style.right=87;
+    clearTimeout(hide);
+            rtxt.style.opacity=1;
+            rall.style.opacity=1;
+            t=true;
+        }
+
+    }
+    function removeLocalTextarea(){
+        document.getElementById('foo').value='';
+        localStorage.removeItem('lcltext');
+        location.reload();
+    }
+    function removeLocals(){
+        document.getElementById('foo').value='';
+        localStorage.removeItem('lcltext');
+        localStorage.removeItem('hmtltseter');
+localStorage.removeItem('mnuul');
+        location.reload();
+    }
+if (!String.prototype.splice) {
+    String.prototype.splice = function(start, delCount, newSubStr) {
+    return this.slice(0, start) + newSubStr + this.slice(start + Math.abs(delCount));
+    };
+}
+//var ad = document.getElementById('audio');
+document.title='HTML Tester';
+var foo=document.getElementById("foo");
+foo.onkeydown=function(e){
+    setTimeout(()=>{localStorage.setItem('lcltext',foo.value);},100);
+    if(e.key=="Tab"){
+        e.preventDefault();
+        var orgin;
+        foo.value=foo.value.splice(orgin=foo.selectionStart,0,'    ');
+        foo.selectionStart=foo.selectionEnd=orgin+4;
+    }
+};
+function test(){
+    //ad.play();
+    var btn = document.getElementById("q").style;
+    var temp=foo.value;
+    var winAcc;
+    try{preWindow.location.host;}catch(a){winAcc=(a&&false);}//jshint ignore:line
+    if(typeof winAcc==='undefined'){winAcc=true;}
+    if(window.preWindow&&(!window.preWindow.closed&&winAcc)){}else{
+        window.preWindow=open();
+    }
+    window.preWindow.document.open();
+    window.preWindow.document.write(temp);
+    window.preWindow.document.close();
+    if(!temp){window.preWindow.location='/dogrolld';}
+    window.preWindow.focus();
+    window.onfocus=()=>{
+    foo.select();
+    };
+}
+mnu=false;
+function openMenu(){
+document.getElementById('instructions').innerHTML='Status;Title;Test;Remove;Trash;Locals;';
+localStorage.setItem('mnuul',true);
+    var title = document.getElementsByClassName('title')[0],tistl=title.style;
+    tistl.height='50%';
+    tistl.fontSize='3vw';
+    tistl.alignItems='flex-start';
+    tistl.justifyContent='flex-end';
+    tistl.padding='10';
+    tistl.top='30px';
+    document.getElementById('a').style.display='block';
+    mnu=true;
+}
+function toggleMenu() {
+if (mnu) {
+    closeMenu();
+} else {
+    if (localStorage.getItem('mnuul')){
+        openMenu();
+    }
+}
+}
+function closeMenu(){
+    if(mnu){
+        var title = document.getElementsByClassName('title')[0],tistl=title.style;
+        tistl.height='20%';
+        tistl.fontSize='6vw';
+        tistl.alignItems='center';
+        tistl.justifyContent='center';
+        tistl.padding='0';
+        var x = document.getElementById('a');
+        x.style.display='none';
+        if(x.value){
+            parseCstm(x.value);
+            localStorage.setItem('hmtltseter',x.value);
+        }
+        mnu=false;
+    }
+}
+var recognition = new webkitSpeechRecognition();
+recognition.onresult = function(event) {
+if(event.results['0']['0'].transcript=="they're taking the Hobbits to Isengard")openMenu();
+};
+recognition.start();
